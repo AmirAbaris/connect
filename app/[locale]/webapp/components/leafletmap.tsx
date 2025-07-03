c5c6b7725cbfcb604ff56a34f05ec0d8cdad6731
Amir Abaris <thisisamirabaris@gmail.com>
2025-07-03 15:47:15 +0330

    feat: add vaul dependency and implement drawer in LeafletMap
    - Added "vaul" as a dependency in package.json and package-lock.json.
    - Integrated a Drawer component in LeafletMap for enhanced user interaction when clicking on the marker.
    - Updated marker event handlers to open the drawer, providing a confirmation interface.

--- app/[locale]/webapp/components/leafletmap.tsx
+++ app/[locale]/webapp/components/leafletmap.tsx
@@ -4,6 +4,17 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import { LatLngExpression } from "leaflet";
+import { useState } from "react";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+} from "@/components/ui/drawer";
+import { Button } from "@/components/ui/button";
 
 interface LeafletMapProps {
   center: LatLngExpression;
@@ -19,21 +30,45 @@ const customIcon = L.icon({
 });
 
 export default function LeafletMap({ center, zoom = 13 }: LeafletMapProps) {
+  const [drawerOpen, setDrawerOpen] = useState(false);
   return (
-    <MapContainer
-      center={center}
-      zoom={zoom}
-      style={{ height: "100%", width: "100%", zIndex: 1 }}
-    >
-      <TileLayer
-        url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
-        attribution='&copy; <a href="https://carto.com/attributions">CARTO</a>'
-      />
+    <>
+      <MapContainer
+        center={center}
+        zoom={zoom}
+        style={{ height: "100%", width: "100%", zIndex: 1 }}
+      >
+        <TileLayer
+          url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+          attribution='&copy; <a href="https://carto.com/attributions">CARTO</a>'
+        />
 
-      {/* Use the custom icon here */}
-      <Marker position={center} icon={customIcon}>
-        <Popup>Your Marker Location</Popup>
-      </Marker>
-    </MapContainer>
+        {/* Use the custom icon here */}
+        <Marker
+          position={center}
+          icon={customIcon}
+          eventHandlers={{
+            click: () => setDrawerOpen(true),
+          }}
+        >
+          <Popup>Your Marker Location</Popup>
+        </Marker>
+      </MapContainer>
+
+      <Drawer open={drawerOpen} onOpenChange={setDrawerOpen}>
+        <DrawerContent>
+          <DrawerHeader>
+            <DrawerTitle>Are you absolutely sure?</DrawerTitle>
+            <DrawerDescription>This action cannot be undone.</DrawerDescription>
+          </DrawerHeader>
+          <DrawerFooter>
+            <Button>Submit</Button>
+            <DrawerClose>
+              <Button variant="outline">Cancel</Button>
+            </DrawerClose>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
   );
 }
