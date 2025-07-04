dddd2f874fde508d8b7997e1050b479646176ac0
Amir Abaris <thisisamirabaris@gmail.com>
2025-07-04 18:59:34 +0330

    feat: add member schema

---
+++ schemas/member-schema.ts
@@ -0,0 +1,24 @@
+import { z } from "zod";
+
+export const memberSchema = z.object({
+  id: z.string(),
+  uid: z.string(),
+  name: z.string().min(1, "نام الزامی است"),
+  age: z.number().min(1, "سن الزامی است"),
+  bio: z.string().nullable().optional(),
+  image: z.string().nullable().optional(),
+  interests: z.array(z.string()).optional(),
+  location: z.string().nullable().optional(),
+});
+
+export const firstFormPageSchema = memberSchema.pick({
+  image: true,
+  name: true,
+  age: true,
+});
+
+export const secondFormPageSchema = memberSchema.pick({
+  bio: true,
+  interests: true,
+  location: true,
+});
