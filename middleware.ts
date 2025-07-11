5f984c19a6d65bd4eacde5d67ccef7b11c884b3e
Amir Abaris <thisisamirabaris@gmail.com>
2025-07-11 17:47:45 +0330

    feat: enable locale detection in middleware and clean up routing
    - Updated `intlMiddleware` to enable locale detection for improved routing.
    - Removed comments from `routing.ts` for a cleaner codebase while maintaining locale definitions.

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import createMiddleware from "next-intl/middleware";
 import { routing } from "./i18n/routing";
 
-const intlMiddleware = createMiddleware(routing);
+const intlMiddleware = createMiddleware({ ...routing, localeDetection: true });
 
 const tokenKey = "sb-dtnknotqorxmauptuipr-auth-token";
 
