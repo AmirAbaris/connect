bab7404d08b86ddc313702e767512a04f8d7c08c
Amir Abaris <thisisamirabaris@gmail.com>
2025-07-11 13:56:28 +0330

    feat: enhance locale layout and metadata generation
    - Added `generateMetadata` function to dynamically create metadata based on locale, utilizing translations for title, description, and keywords.
    - Updated `routing` to support English locale alongside Farsi.
    - Modified `LocaleLayout` to set HTML language and direction based on locale, improving accessibility and SEO.

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -2,6 +2,62 @@ import { NextIntlClientProvider, hasLocale } from "next-intl";
 import { notFound } from "next/navigation";
 import { routing } from "@/i18n/routing";
 import "./../globals.css";
+import type { Metadata } from "next";
+import { getTranslations } from "next-intl/server";
+
+const defaultUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : "http://localhost:3000";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { locale: string };
+}): Promise<Metadata> {
+  const { locale } = params;
+  if (!hasLocale(routing.locales, locale)) {
+    return {};
+  }
+  const t = await getTranslations({ locale });
+  // Use t for all metadata fields
+  const title = `Connect - ${t("Hero.title")}`;
+  const description = t("Footer.description");
+  const keywords = t("Footer.descriptionShort").split(" ");
+  const isFa = locale === "fa";
+  return {
+    metadataBase: new URL(defaultUrl),
+    title,
+    description,
+    keywords,
+    openGraph: {
+      title,
+      description,
+      url: defaultUrl,
+      siteName: title,
+      locale: isFa ? "fa_IR" : "en_US",
+      type: "website",
+    },
+    twitter: {
+      card: "summary",
+      title,
+      description,
+    },
+    robots: {
+      index: true,
+      follow: true,
+      googleBot: {
+        index: true,
+        follow: true,
+        "max-video-preview": -1,
+        "max-image-preview": "large",
+        "max-snippet": -1,
+      },
+    },
+    verification: {
+      google: "your-google-verification-code",
+    },
+  };
+}
 
 export default async function LocaleLayout({
   children,
@@ -10,11 +66,17 @@ export default async function LocaleLayout({
   children: React.ReactNode;
   params: Promise<{ locale: string }>;
 }) {
-  // Ensure that the incoming `locale` is valid
   const { locale } = await params;
   if (!hasLocale(routing.locales, locale)) {
     notFound();
   }
 
-  return <NextIntlClientProvider>{children}</NextIntlClientProvider>;
+  const isFa = locale === "fa";
+  return (
+    <html lang={locale} dir={isFa ? "rtl" : "ltr"} suppressHydrationWarning>
+      <body>
+        <NextIntlClientProvider>{children}</NextIntlClientProvider>
+      </body>
+    </html>
+  );
 }
