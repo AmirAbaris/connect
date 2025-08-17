16961f87fb8f9f0af6ffd6dff4db56af87c8da5e
Amir Abaris <thisisamirabaris@gmail.com>
2025-08-17 12:20:12 +0330

    feat: i18n on reset password page

--- app/[locale]/[auth]/reset-password/page.tsx
+++ app/[locale]/[auth]/reset-password/page.tsx
@@ -7,8 +7,12 @@ import { resetPasswordUserSchema } from "@/schemas/user-schema";
 import { AuthResetPasswordType } from "@/types/user";
 import useAuth from "@/hooks/use-auth";
 import { useForm } from "react-hook-form";
+import { useTranslations } from "next-intl";
+import { Eye, EyeOff, Loader2 } from "lucide-react";
+import { useState } from "react";
 
 export default function ResetPasswordPage() {
+  const t = useTranslations();
   const {
     register,
     handleSubmit,
@@ -17,6 +21,7 @@ export default function ResetPasswordPage() {
     resolver: zodResolver(resetPasswordUserSchema),
   });
   const { resetPassword, isPendingResetPassword } = useAuth();
+  const [showPassword, setShowPassword] = useState<boolean>(false);
 
   const onSubmit = (data: AuthResetPasswordType) => {
     resetPassword(data);
@@ -28,21 +33,37 @@ export default function ResetPasswordPage() {
         onSubmit={handleSubmit(onSubmit)}
         className="bg-card/50 backdrop-blur-sm border border-border/50 rounded-2xl p-8 shadow-xl w-full max-w-md space-y-6"
       >
-        <h2 className="text-2xl font-bold text-center mb-4">تغییر رمز عبور</h2>
-        <Input
-          type="password"
-          placeholder="رمز عبور جدید"
-          {...register("newPassword")}
-          required
-          minLength={8}
-          className="py-3 text-right"
-        />
+        <h2 className="text-2xl font-bold text-center mb-4">
+          {t("ResetPassword.changePassword")}
+        </h2>
+        <div className="relative">
+          <Input
+            placeholder={t("ResetPassword.newPassword")}
+            {...register("newPassword")}
+            required
+            minLength={8}
+            className="py-3"
+            type={showPassword ? "text" : "password"}
+          />
+          <Button
+            type="button"
+            variant="ghost"
+            className="absolute right-3 top-0"
+            onClick={() => setShowPassword(!showPassword)}
+          >
+            {showPassword ? <EyeOff /> : <Eye />}
+          </Button>
+        </div>
         <Button
           type="submit"
           className="w-full"
           disabled={isPendingResetPassword || !isValid}
         >
-          {isPendingResetPassword ? "در حال تغییر..." : "تغییر رمز عبور"}
+          {isPendingResetPassword ? (
+            <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+          ) : (
+            t("ResetPassword.changePassword")
+          )}
         </Button>
       </form>
     </div>
